bdd9ce35ed1c8b465b101ec924043f835cf9487f
ThangTV <thangchien86@gmail.com>
2024-05-12 23:17:13 +0700

    Update Code

---
+++ src/components/mintNFTPage/mintNFTStyle.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+const mintNFTStyle = styled.div`
+  width: 100%;
+  margin: 10px;
+  .gird {
+    .css-1v2c24a-MuiGrid-root {
+      margin: 0;
+    }
+  }
+`
+
+export default mintNFTStyle;
