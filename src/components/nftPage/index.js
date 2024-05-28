0b9c0db81128b22a72b9a5beb029874161a755b3
Thang <thangchien86@gmail.com>
2024-05-28 18:33:33 +0700

    Update Code

--- src/components/nftPage/index.js
+++ src/components/nftPage/index.js
@@ -23,21 +23,28 @@ import {
   Tab,
   CardHeader,
   CardMedia,
-  Avatar
+  Avatar,
+  Tooltip
 } from '@mui/material'
 import DetailNFTStyle from './DetailNFTStyle'
-import SolanaIcon from '../../images/logos/SolanaIcon.svg';
-import {useSearchQuery} from "../../utils/helpers"
+import SolanaIcon from '../../images/logos/SolanaIcon.svg'
+import { useSearchQuery } from '../../utils/helpers'
 const DetailNFT = () => {
-const params = useSearchQuery();
-  console.log(params);
+  const params = useSearchQuery()
+  console.log(params)
   return (
     <DetailNFTStyle>
       <div className='itemNFT'>
         <div className='descriptionNFT'>
           <div className='imageNFT'>
             <Card variant='outlined' className=''>
-              <CardHeader avatar={<Avatar aria-label='recipe' src={SolanaIcon}></Avatar>}></CardHeader>
+              <CardHeader
+                avatar={
+                  <Tooltip title='Solana Chain' placeholder='bottom' arrow>
+                    <Avatar aria-label='recipe' src={SolanaIcon}></Avatar>
+                  </Tooltip>
+                }
+              ></CardHeader>
               <CardMedia
                 lt='fasdf'
                 image='https://pantravel.vn/wp-content/uploads/2023/11/dong-thac-trang-xoa-do-xuong.jpg'
@@ -50,20 +57,20 @@ const params = useSearchQuery();
             <div className='label'>Description</div>
             <div className='creatorNFT'>By adsfsasdf</div>
             <div className='detailsNFT'></div>
-        </div>
+          </div>
         </div>
         <div className='marketNFT'>
-            <div className='colationName'></div>
-            <div className='ownerNFT'>Owner By fsadfsdf</div>
-            <div className='catergoryNFT'>PFPs</div>
-            <div className='listingNFT'>
-                <TextField></TextField>
-                <div className='priceNFT'>3 SOL</div>
-                <div className=''>
-                    <div className='priceNFT'>Buy </div>
-                    <div className='offerNFT'>Make offer</div>
-                </div>
+          <div className='colationName'></div>
+          <div className='ownerNFT'>Owner By fsadfsdf</div>
+          <div className='catergoryNFT'>PFPs</div>
+          <div className='listingNFT'>
+            <TextField></TextField>
+            <div className='priceNFT'>3 SOL</div>
+            <div className=''>
+              <div className='priceNFT'>Buy </div>
+              <div className='offerNFT'>Make offer</div>
             </div>
+          </div>
         </div>
       </div>
     </DetailNFTStyle>
